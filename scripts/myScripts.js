27dfcdb5ff0dcceeb94376d868669e2da05b6ce9
desya42 <59778932+desya42@users.noreply.github.com>
2020-03-09 20:54:08 -0500

    Add files via upload

--- scripts/myScripts.js
+++ scripts/myScripts.js
@@ -1,24 +1,28 @@
+/*Andrew Folkerts JS
+    Created 3/09/20
+    Version: 1.0*/
+
 console.log("Waiting on user name");
 //function that delivers a greeting
 function greeting() {
-  console.log("user put in name ");
-  console.log("starting to have fun");
-  var inputName = document.getElementById("nameInput");
-  var outputGreet = document.getElementById("outputGreeting");
-  var name = nameInput.value;
-  console.log("user submitted name: " + name);
-  console.log("outputting greeting message");
-  outputGreet.value = "Hello, " + name + " nice to meet you!";
-  console.log("user can see the greeting");
+    console.log("user put in name ");
+    console.log("starting to have fun");
+    var inputName = document.getElementById("nameInput");
+    var outputGreet = document.getElementById("outputGreeting");
+    var name = nameInput.value;
+    console.log("user submitted name: " + name);
+    console.log("outputting greeting message");
+    outputGreet.value = "Hello, " + name + " nice to meet you!";
+    console.log("user can see the greeting");
 }
 //function to count number of letters
 function count() {
-  console.log("starting to count letters");
-  var inputName = document.getElementById("nameInput");
-  var outCount = document.getElementById("outCount");
-  var name = nameInput.value;
-  var count = name.length;
-  console.log("outputting letter count message");
-  outCount.value = "You have " + count + " letters and spaces in your name";
-  console.log("user can see the count message");
+    console.log("starting to count letters");
+    var inputName = document.getElementById("nameInput");
+    var outCount = document.getElementById("outCount");
+    var name = nameInput.value;
+    var count = name.length;
+    console.log("outputting letter count message");
+    outCount.value = "You have " + count + " letters and spaces in your name";
+    console.log("user can see the count message");
 }
